51a5f1441577a1bd46bc12a366062d224be2767d
JianKiat <58556166+JianKiat@users.noreply.github.com>
2025-07-19 23:07:56 +0800

    update layout with sidebar and topbar

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,4 +1,12 @@
-import "./globals.css"; // Adjust path accordingly
+import "./globals.css";
+import { AppSidebar } from "@/components/ui/sidebar/app-sidebar";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppBreadcrumb } from "@/components/ui/topbar/breadcrumb";
 
 export default function RootLayout({
   children,
@@ -7,7 +15,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        {/* Add any meta tags, title, or links here */}
+      </head>
+      <body>
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator
+                  orientation="vertical"
+                  className="mr-2 data-[orientation=vertical]:h-4"
+                />
+                <AppBreadcrumb />
+              </div>
+            </header>
+            {children}
+          </SidebarInset>
+        </SidebarProvider>
+      </body>
     </html>
   );
 }
