0da372ffdf049c8a7714dbed3d0cbc4400b66c04
Yew Kit <Yew_Kit_Leong_from.TP@mom.gov.sg>
2025-07-22 19:30:51 +0800

    database

---
+++ server/app/database/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+Base = declarative_base()
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Dependency function to get the database session
+def get_db() -> Session:  # type: ignore
+    try:
+        db = SessionLocal()
+    except Exception as e:
+        raise e
+    try:
+        yield db
+    finally:
+        db.close()
+        
+
