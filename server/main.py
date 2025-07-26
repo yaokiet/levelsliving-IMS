780f0f65093c0015c5e5e36fc149843c576bfc80
JianKiat <58556166+JianKiat@users.noreply.github.com>
2025-07-26 22:47:07 +0800

    add authmiddleware to main.py

--- server/main.py
+++ server/main.py
@@ -1,12 +1,11 @@
-
-from starlette.middleware.sessions import SessionMiddleware
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.v1.router import router as v1_router  # ✅ Correct import
+from api.v1.router import router as v1_router
+from app.auth.auth_middleware import AuthMiddleware 
 
 app = FastAPI()
 
-app.add_middleware(SessionMiddleware, secret_key="estabsecret", session_cookie="session_id", max_age=3600)
+app.add_middleware(AuthMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
