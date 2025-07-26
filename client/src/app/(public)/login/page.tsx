10db9622618fe278ee1711e49bf36442be1e6c59
JianKiat <58556166+JianKiat@users.noreply.github.com>
2025-07-26 11:31:45 +0800

    add login page

---
+++ client/src/app/(public)/login/page.tsx
@@ -0,0 +1,39 @@
+import { GalleryVerticalEnd } from "lucide-react"
+
+import { LoginForm } from "@/components/login/login-form"
+import { ModeToggle } from "@/components/ui/topbar/toggle-theme"
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            <div className="flex flex-col gap-0.5 leading-none">
+              <span className="font-medium">Levels Living</span>
+              <span className="">IMS</span>
+            </div>
+          </a>
+          <div className="ml-auto">
+              <ModeToggle />
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/login-img.jpg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.7]"
+        />
+      </div>
+    </div>
+  )
+}
