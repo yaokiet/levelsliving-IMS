b35e39cbee5b20e3e8fc573a2e9d1d1c21078281
JianKiat <58556166+JianKiat@users.noreply.github.com>
2025-08-02 17:46:12 +0800

    add redux provider to root layout

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import ReduxProvider from "./_store/ReduxProvider";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Add any meta tags, title, or links here */}
+      </head>
+      <body>
+        <ReduxProvider>
+          {children}
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+}
