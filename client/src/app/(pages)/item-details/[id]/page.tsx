0a05157bfd70a76ae5e676910d837d486e84f9cb
JianKiat <58556166+JianKiat@users.noreply.github.com>
2025-08-03 00:03:29 +0800

    update test getAllItem

--- client/src/app/(pages)/item-details/[id]/page.tsx
+++ client/src/app/(pages)/item-details/[id]/page.tsx
@@ -1,6 +1,9 @@
-import React from "react"
+"use client"
+
+import React, { useEffect } from "react"
 import { ItemInfoCard } from "@/components/ui/item/item-info-card"
 import TableSection from "@/components/table/table-section"
+import { getAllItems } from "@/lib/api/itemsApi"
 
 export default function ItemDetailsPage() {
   // Example item data
@@ -11,6 +14,18 @@ export default function ItemDetailsPage() {
     type: "Gadget",
     quantity: 42,
   }
+
+  useEffect(() => {
+    try {
+      getAllItems().then(items => {
+        console.log("Fetched items:", items)
+      })
+    }
+    catch (error) {
+      console.error("Error fetching items:", error)
+    }
+
+  }, []);
 
   return (
     <div className="container mx-auto py-10 px-6">
