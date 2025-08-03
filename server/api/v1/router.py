0eadd54ef979e728ea773efc14b12f4e3edf6f85
JoelSngZhenWei <joelsngzw@gmail.com>
2025-08-04 01:49:02 +0800

    Checking deployment

--- server/api/v1/router.py
+++ server/api/v1/router.py
@@ -14,7 +14,7 @@ router.include_router(auth_router)
 @router.get("/test")
 def test():
     """
-    Test endpoint to check if the server is running.
+    Test endpoint to check if the server is running. Double check at this point.
     """
     return {"message": "Server is running!"}
 
