aa304bb3321c4865d9bb8750238cd24b933cc3d0
JiaweiSMU <jiawei.chua.2022@scis.smu.edu.sg>
2025-09-06 16:04:51 +0800

    Integrated Orders with backend
    - Fix the data type return
    - Not yet implement pagination logic

--- client/src/components/table/reusable/reusable-table.tsx
+++ client/src/components/table/reusable/reusable-table.tsx
@@ -43,7 +43,6 @@ interface ReusableTableProps<TData extends Record<string, any>, TValue> {
   renderSubRows?: (row: any, colSpan: number) => React.ReactNode;
   className? : string;
   containerClassName?: string;
-  renderToolbarExtras?: (ctx: { table: ReturnType<typeof useReactTable<TData>> }) => React.ReactNode;
 }
 
 export function ReusableTable<TData extends Record<string, any>, TValue>({
@@ -59,7 +58,6 @@ export function ReusableTable<TData extends Record<string, any>, TValue>({
   className = "",
   containerClassName = "w-5/5 rounded-xl shadow-xl p-8",
   renderSubRows,
-  renderToolbarExtras,
 }: ReusableTableProps<TData, TValue>) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
@@ -157,12 +155,6 @@ return (
                   });
                 }}
               />
-            </div>
-          )}
-          {/* Render additional toolbar for Add to Cart button */}
-          {renderToolbarExtras && (
-            <div className="ml-auto flex items-center gap-3">
-              {renderToolbarExtras({ table })}
             </div>
           )}
           {showViewOptions && (
