da4d1fcf13759acda617ea3c6bf09e6cd52abc3e
JianKiat <58556166+JianKiat@users.noreply.github.com>
2025-09-13 15:57:11 +0800

    add cartcheckout to redux

--- client/src/app/_store/redux-store.ts
+++ client/src/app/_store/redux-store.ts
@@ -2,17 +2,25 @@ import { configureStore } from "@reduxjs/toolkit"
 import { persistStore, persistReducer } from "redux-persist"
 import storage from "redux-persist/lib/storage"
 import authReducer from "./authSlice"
+import cartCheckoutReducer from "./cartCheckoutSlice";
 
 const persistConfig = {
   key: "auth",
   storage,
 }
 
+const cartCheckoutPersistConfig = {
+  key: "cartCheckout",
+  storage,
+};
+
 const persistedReducer = persistReducer(persistConfig, authReducer)
+const persistedCartCheckoutReducer = persistReducer(cartCheckoutPersistConfig, cartCheckoutReducer);
 
 export const store = configureStore({
   reducer: {
     auth: persistedReducer,
+    cartCheckout: persistedCartCheckoutReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
