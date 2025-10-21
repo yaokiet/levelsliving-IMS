7be7803c418d8b777fb7a645dc47fab699352a1f
JianKiat <58556166+JianKiat@users.noreply.github.com>
2025-10-21 22:30:29 +0800

    test client cicd

--- client/src/app/(pages)/orders/page.tsx
+++ client/src/app/(pages)/orders/page.tsx
@@ -7,6 +7,7 @@ import OrderPageTable from "@/components/table/order/order-page-table";
 export default function OrdersPage() {
   return (
     <div className="container mx-auto py-10 px-6">
+      <h2 className="text-2xl font-bold">Orders</h2>
       <OrderPageTable />
     </div>
   );
