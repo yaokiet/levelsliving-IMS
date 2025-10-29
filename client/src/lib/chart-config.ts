ed00c35ae715100144a8247847278f2350adc27a
JoelSngZhenWei <joelsngzw@gmail.com>
2025-10-29 14:52:50 +0800

    Refactored code

---
+++ client/src/lib/chart-config.ts
@@ -0,0 +1,12 @@
+import { ChartConfig } from "@/components/ui/chart";
+
+export const chartConfig: ChartConfig = {
+    quantity: {
+        label: "Quantity",
+        color: "var(--chart-1)",
+    },
+    prediction: {
+        label: "Prediction",
+        color: "var(--chart-2)",
+    },
+};
