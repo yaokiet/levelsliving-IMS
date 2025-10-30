3010c152ac7289ca7799eb590a0dff0434bd77f7
JianKiat <58556166+JianKiat@users.noreply.github.com>
2025-10-30 17:50:38 +0800

    comment out lark

--- server/config.py
+++ server/config.py
@@ -8,10 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent
 
 class Settings(BaseSettings):
     # Lark
-    LARK_APP_ID: str
-    LARK_APP_SECRET: str
-    LARK_SPREADSHEET_ID: str
-    LARK_BASE_URL: str = "https://open.larksuite.com/open-apis"
+    # LARK_APP_ID: str
+    # LARK_APP_SECRET: str
+    # LARK_SPREADSHEET_ID: str
+    # LARK_BASE_URL: str = "https://open.larksuite.com/open-apis"
 
     # Mail
     mail_username: str
